9b4d6ca4d9725c2d63847d93182a6002b2cf6ff0
Javier Fonseca <102896422+Javi-Fonseca@users.noreply.github.com>
2023-12-15 12:17:25 -0600

    Basic Operations on Numbers

---
+++ FreeCodeCamp-Microsoft/project-StudentGrades.cs
@@ -0,0 +1,66 @@
+// Visual Studio Code, provide tools that enable developers to observe the values of application variables while their code is running. 
+//Learning how to use those tools saves professional developers lots of time during development. 
+// initialize variables - graded assignments 
+/*
+You're given a short list of four students and their five assignment grades.
+Each assignment grade is expressed as an integer value, 0-100, where 100 represents 100% correct.
+Final scores are calculated as an average of the five assignment scores.
+Your application needs to perform basic math operations to calculate the final grades for each student.
+Your application needs to output/display each student’s name and final score.
+
+ calculated grades for each student are displayed as follows:
+Student     Grade
+Sophia      94.6  A
+Nicolas     83.6  B
+Zahirah     83.4  B
+Jeong       95.4  A
+
+grading scale that the teacher uses to assign letter grades:
+97 - 100    A+
+93 - 96     A
+90 - 92     A-
+87 - 89     B+
+83 - 86     B
+*/
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+decimal sophiaScore = (decimal) sophiaSum / currentAssignments;
+decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
+decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
+decimal jeongScore = (decimal) jeongSum / currentAssignments;
+
+Console.WriteLine("Student Grade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
+Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
+Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
+Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
+//Printing a tab fills variable length whitespace until a position divisible by 4 is reached
