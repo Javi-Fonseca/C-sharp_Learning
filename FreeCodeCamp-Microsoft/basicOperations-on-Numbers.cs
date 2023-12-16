c6964a336667cf88c649a52de69d94ce0345994d
Javier Fonseca <102896422+Javi-Fonseca@users.noreply.github.com>
2023-12-16 15:10:35 -0600

    Update files
    project-StudentGrades.cs update
    basicOperations-on-Numbers.cs update
    basicString-Formatting.cs update

--- FreeCodeCamp-Microsoft/basicOperations-on-Numbers.cs
+++ FreeCodeCamp-Microsoft/basicOperations-on-Numbers.cs
@@ -1,106 +1,108 @@
 //  https://learn.microsoft.com/en-us/training/modules/csharp-basic-operations/
-//      Basic operations on numbers     2023.12.14
+//    Basic operations on numbers     2023.12.14
 using System;
 
 public class Program {
     public static void Main (string[] args) {
-        /*  Unit 2 Overloading the operator
-              This means when one symbol has multiple purposes.
-                - '+' : It's used as addition of numbers and 
-                        as concatenation  of strings.
-                - '()' : They are used as invocation of method,
-                        as order of operations for operators,
-                        as casting data.
-        */
+      /*Unit 2 Overloading the operator
+          This means when one symbol has multiple purposes.
+            - '+' : It's used as addition of numbers and 
+                    as concatenation  of strings.
+            - '()' : They are used as invocation of method,
+                    as order of operations for operators,
+                    as casting data.
+      */
+      string firstName = "Yuuka";
+      int level = 25 ; 
+      int levelUpper = 3 ;
+      Console.WriteLine(firstName + " is level " + level); 
+      // C-sharp compiler implicity convert an 'int' value into a 'string' value for concatenation.
+      Console.writeLine(firstName + " has improve her level to " + level + levelUpper);
+      Console.writeLine(firstName + " has improve her level to " + (level + levelUpper));
+      // Use '()' to declare the order of operations.
 
-        string firstName = "Yuuka";
-        int level = 25 ; 
-        int levelUpper = 3 ;
-        Console.WriteLine(firstName + " is level " + level); 
-        // C-sharp compiler implicity convert an 'int' value into a 'string' value for concatenation.
-        Console.writeLine(firstName + " has improve her level to " + level + levelUpper);
-        Console.writeLine(firstName + " has improve her level to " + (level + levelUpper));
-        // Use '()' to declare the order of operations.
 
-        /* Unit 3  Basic math operators
-             - '-' : is the subtraction operator
-             - '+' : is the addition operator
-             - '*' : is the multiplication operator
-             - '/' : is the division operator
-             - '%' : is the remainder operator
-        */
-        /* More Data types
-        integer : use 'int' to declarate a integer value.
-        decimal : use 'decimal' and 'm' to declarate a floating point number value.
-                  The number (left of the assignment operator) should be type decimal.
+      /* Unit 3  Basic math operators
+         - '-' : is the subtraction operator
+         - '+' : is the addition operator
+         - '*' : is the multiplication operator
+         - '/' : is the division operator
+         - '%' : is the remainder operator
+      */
+      /*More Data types
+          integer : use 'int' to declarate a integer value.
+          decimal : use 'decimal' and 'm' to declarate a floating point number value.
+                    The number (left of the assignment operator) should be type decimal.
 
-           Data Conversion
-        Casting is a type of data conversion, that allows temporarily treat a value as
-        a different data type.
-          -Use '(datatype)' in front of the value (before or right side) to cast it.
+        Data Conversion
+          Casting is a type of data conversion, that allows temporarily treat a value as
+          a different data type.
+            -Use '(datatype)' in front of the value (before or right side) to cast it.
 
-          Order of operations
-          -PEMDAS
-          Parenthesis
-          Exponents
-          Multiplication and Division (from left to right)
-          Addition and Subtraction (from left to right)
+        Order of operations
+          -PEMDAS:
+          1.Parenthesis
+          2.Exponents
+          3.Multiplication and Division (from left to right)
+          4.Addition and Subtraction (from left to right)
 
           C-sharp does not have exponent operator. It's common use the 
           System.Math.Pow method, from the .NET Class library.
-        */
-        int sum = 4+2;
-        int difference = 3-2;
-        int product = 5*6;
-        int quotient = 8/7;
-        int remainder = 200%3;
-        Console.WriteLine("Sum: " + sum);
-        Console.WriteLine("Difference: " + difference);
-        Console.WriteLine("Product: " + product);
-        Console.WriteLine("Quotient: " + quotient);
-        Console.WriteLine($"Remainder:  {remainder}");
-        Console.WriteLine($"Quotient(decimal): {(decimal)7/5}"); // casting
-        Console.WriteLine($"Quotient(decimal): {7.0m/5}"); //decimal type
-        Console.WriteLine(2+(3*5/4)-1); //PEMDAS
+      */        
+      int sum = 4+2;
+      int difference = 3-2;
+      int product = 5*6;
+      int quotient = 8/7;
+      int remainder = 200%3;
+      Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Difference: " + difference);
+      Console.WriteLine("Product: " + product);
+      Console.WriteLine("Quotient: " + quotient);
+      Console.WriteLine($"Remainder:  {remainder}");
+      Console.WriteLine($"Quotient(decimal): {(decimal)7/5}"); // casting
+      Console.WriteLine($"Quotient(decimal): {7.0m/5}"); //decimal type
+      Console.WriteLine(2+(3*5/4)-1); //PEMDAS
 
-        /* Unit 4 Compound Assignment
+
+      /* Unit 4 Compound Assignment
          They compound some operation in addition to assigning the result to the variable.
         - '-=', '+=', '*=', '/='.
          - '--' and '++' : decrement  and increment value, can be assign 
          before or after the value for affect performing of retrieve.
-        */
-        int numberA = 10;
-        numberA -= 5;
-        numberA += 3;
-        numberA *= 7;
-        numberA /= 7;
-        Console.WriteLine("The grade note is: " + (++numberA));
-        Console.WriteLine("I repeat, the grade note is: " + (numberA++));
-        Console.WriteLine("The new grade note is: " + (numberA));
-        Console.WriteLine("Nevermind, The grade note is: " + (--numberA));
-        // Use '()' arround the expression '(++numberA)' improve readability.
+      */  
+      int numberA = 10;
+      numberA -= 5;
+      numberA += 3;
+      numberA *= 7;
+      numberA /= 7;
+      Console.WriteLine("The grade note is: " + (++numberA));
+      Console.WriteLine("I repeat, the grade note is: " + (numberA++));
+      Console.WriteLine("The new grade note is: " + (numberA));
+      Console.WriteLine("Nevermind, The grade note is: " + (--numberA));
+      // Use '()' arround the expression '(++numberA)' improve readability.
 
-        /* Challenge
+
+      /* Challenge
         Convert fahrenheit to Celsius
-        This must be the output:
+        Expected output:
         The temperature is 34.444444444444444444444444447 Celsius.
-        */
-        //To convert Fahrenheit to Celsius, first subtract 32, then multiply by five ninths (5 / 9).
-        int fahrenheit = 94;
-        int celsius = 0;
-        decimal convertionFtoC = (fahrenheit - 32) * (5.0m / 9.0m);
-        decimal convertionCtoF = (celsius / (5.0m / 9.0m)) + 32;
-        Console.WriteLine($"The temperature is {celsius} Celsius.");
-        Console.WriteLine($"or the temperature is {fahrenheit} Celsius.");
+      */
+      //To convert Fahrenheit to Celsius, first subtract 32, then multiply by five ninths (5 / 9).
+      int fahrenheit = 94;
+      int celsius = 0;
+      decimal convertionFtoC = (fahrenheit - 32) * (5.0m / 9.0m);
+      decimal convertionCtoF = (celsius / (5.0m / 9.0m)) + 32;
+      Console.WriteLine($"The temperature is {celsius} Celsius.");
+      Console.WriteLine($"or the temperature is {fahrenheit} Celsius.");
 
-        decimal fahrenheit2 = 94;
-        decimal celsius2 = 0;
-        decimal convertionFtoC2 = (fahrenheit2 - 32) * (5.0m / 9.0m);
-        celsius2 = convertionFtoC2;
-        decimal convertionCtoF2 = (celsius2 / (5.0m / 9.0m)) + 32;
-        fahrenheit2 = convertionCtoF2;
-        Console.WriteLine($"The temperature is {celsius2} Celsius.");
-        Console.WriteLine($"or the temperature is {fahrenheit2} fahrenheit.");
+      decimal fahrenheit2 = 94;
+      decimal celsius2 = 0;
+      decimal convertionFtoC2 = (fahrenheit2 - 32) * (5.0m / 9.0m);
+      celsius2 = convertionFtoC2;
+      decimal convertionCtoF2 = (celsius2 / (5.0m / 9.0m)) + 32;
+      fahrenheit2 = convertionCtoF2;
+      Console.WriteLine($"The temperature is {celsius2} Celsius.");
+      Console.WriteLine($"or the temperature is {fahrenheit2} fahrenheit.");
 
     }
 }
--- FreeCodeCamp-Microsoft/project-StudentGrades.cs
+++ FreeCodeCamp-Microsoft/project-StudentGrades.cs
@@ -1,27 +1,38 @@
-// Visual Studio Code, provide tools that enable developers to observe the values of application variables while their code is running. 
-//Learning how to use those tools saves professional developers lots of time during development. 
-// initialize variables - graded assignments 
-/*
-You're given a short list of four students and their five assignment grades.
-Each assignment grade is expressed as an integer value, 0-100, where 100 represents 100% correct.
-Final scores are calculated as an average of the five assignment scores.
-Your application needs to perform basic math operations to calculate the final grades for each student.
-Your application needs to output/display each student’s name and final score.
+//  Project: Students Grades    2023.12.14 
+using System;
 
- calculated grades for each student are displayed as follows:
+public class studentGrades {
+    public static void Main (string[] args) {
+/*
+  Parameters:
+-You're given a short list of four students and their five assignment
+ grades.
+-Each assignment grade is expressed as an integer value, 0-100, where
+ 100 represents 100% correct.
+-Final scores are calculated as an average of the five assignment
+ scores.
+-Your application needs to perform basic math operations to calculate
+ the final grades for each student.
+-Your application needs to output/display each student’s name and
+ final score.
+
+Grading scale that the teacher uses to assign letter grades:
+97 - 100    A+
+93 - 96     A
+90 - 92     A-
+87 - 89     B+
+83 - 86     B
+
+  Output
+Calculate grades for each student and display them as follows:
 Student     Grade
 Sophia      94.6  A
 Nicolas     83.6  B
 Zahirah     83.4  B
 Jeong       95.4  A
 
-grading scale that the teacher uses to assign letter grades:
-97 - 100    A+
-93 - 96     A
-90 - 92     A-
-87 - 89     B+
-83 - 86     B
 */
+// Datas
 int currentAssignments = 5;
 
 int sophia1 = 93;
@@ -48,6 +59,7 @@ int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
 
+// Operations
 int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
 int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
 int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
@@ -58,9 +70,11 @@ decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
 decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
 decimal jeongScore = (decimal) jeongSum / currentAssignments;
 
+// Output
 Console.WriteLine("Student Grade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
 Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
 Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
 Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
-//Printing a tab fills variable length whitespace until a position divisible by 4 is reached
+    }
+}
